185792fea1a33fa371c9363b32e261e532fb10e0
gerchenceto <gergana@tradeo.com>
2014-02-20 21:42:23 +0200

    Add Score and Timer

--- SuperMario/SuperMario/Score.cs
+++ SuperMario/SuperMario/Score.cs
@@ -4,5 +4,70 @@
 
     public class Score
     {
+        private const int INCREMENT_VALUE = 10;
+
+        private double currentScore;
+        private double finalscore;
+
+
+        public Score()
+        {
+            currentScore = 0;
+        }
+
+        public double CurrentScore
+        {
+            get 
+            { 
+                return currentScore;
+            }
+            set
+            {
+                currentScore = value;
+            }
+        }
+
+        public double Finalscore
+        {
+            get 
+            { 
+                return finalscore;
+            }
+            set
+            {
+                finalscore = value;
+            }
+        }
+
+        public double get()
+        {
+            return currentScore;
+        }
+
+        //adds 10 points to the current score (on a new correctly guessed word position)
+        public void increment()
+        {
+            currentScore += INCREMENT_VALUE;
+            finalscore += INCREMENT_VALUE;
+        }
+
+        //penalty - lose half of the points (when you hit a brick?)
+        public void decrement()
+        {
+            if (currentScore > 0)
+            {
+                currentScore /= 2;
+                Finalscore /= 2;
+            }
+        }
+
+        public void print()
+        {
+            //Console.SetCursorPosition();
+            Console.CursorVisible = false;
+            Console.WriteLine("Score: {0,-10}", currentScore);
+            //Console.SetCursorPosition();
+            Console.WriteLine("Total: {0,-10}", Finalscore);
+        }
     }
 }
--- SuperMario/SuperMario/Timer.cs
+++ SuperMario/SuperMario/Timer.cs
@@ -4,5 +4,83 @@
 
     public class Timer
     {
+        private const int DEFAULT_GAME_TIME = 90; // in seconds 
+   
+        private int timeInterval;
+        private int startTime;
+        private int currentTime;
+        private int endTime;
+        private int timeLeft;
+
+        public Timer()
+        {
+            initTimer(DEFAULT_GAME_TIME);
+        }
+
+        public Timer(int time) // Timer t = new Timer(40, 60, 8) - if we want to reduce the time for next levels;
+        {
+            initTimer(time);
+        }
+
+        private void initTimer(int time)
+        {
+            timeInterval = time;
+
+            startTime = getCurrentTime();
+            currentTime = startTime;
+            endTime = startTime + time;
+            //Console.WriteLine("Start time is: " + startTime);
+            //Console.WriteLine(endTime);
+        }
+
+        private int getCurrentTime()
+        {
+            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            return (int)t.TotalSeconds;
+        }
+
+        private bool checkTick()
+        {
+            if (getCurrentTime() > currentTime)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool updateTimer()
+        {
+            if (checkTick())
+            {
+                currentTime += 1; // increment with 1 second
+                timeLeft = timeInterval - (currentTime - startTime);
+
+                if (timeLeft >= 0)
+                {
+                    //Console.SetCursorPosition();
+                    Console.CursorVisible = false;
+                    Console.WriteLine("Time: {0,-5}", timeLeft);
+                    if (timeLeft > 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false; // NOTE: We return false only if time is over!
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
     }
 }
