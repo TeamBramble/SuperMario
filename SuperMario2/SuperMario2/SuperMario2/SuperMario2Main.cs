b34c8bb41f76b45aeaf35c28c77fa38258dd6244
deyantodorov <deyan.todorov@hotmail.com>
2014-02-23 09:48:25 +0200

    SuperMario2 - simplified version with ASCII heroes
    This is attempt for simplified version.

---
+++ SuperMario2/SuperMario2/SuperMario2/SuperMario2Main.cs
@@ -0,0 +1,15 @@
+﻿namespace SuperMario2
+{
+    using System;
+    using System.Linq;
+
+    public class SuperMario2Main
+    {
+        /// <summary>
+        /// Here we setup game start
+        /// </summary>
+        private static void Main()
+        {
+        }
+    }
+}
